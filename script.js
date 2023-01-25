51c8edf1c1133f3ea2141a90d1108e30fa47f63d
rkarmadhn <rakaaputtra1@gmail.com>
2023-01-26 00:56:55 +0700

    Fix

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 const navbar = document.querySelector("#navbar");
 const navbarComponent = `<div class="container-xl">
     <div class="navbar-brand">
-      <a href="http://"><img src="images/logo-black.png" alt="logo-black" /></a>
+      <a href="http://"><img src="images/logo-title.png" alt="logo-black"/></a>
     </div>
     <div class="navbar-items">
       <div class="head-menu">
       <div class="head-menu-logo">
-      <a href="http://"><img src="images/logo-black.png" alt="logo-black" /></a>
+      <a href="http://"><img src="images/logo-title.png" alt="logo-black"/></a>
         </div>
         <button class="btn-hide"><i class="fa-solid fa-xmark"></i></button>
       </div>
